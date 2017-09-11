f6aa2d34750483ddb3d6a69a286c792ff2229027
Krisztian Szucs <szucs.krisztian0818@gmail.com>
2017-09-11 14:51:14 +0200

    feat(variable_mutation)exercise done

--- week-02/day-01/variable_mutation.py
+++ week-02/day-01/variable_mutation.py
@@ -1,6 +1,7 @@
 a = 3
 # make it bigger by 10
 
+a += 10
 
 print(a)
 
@@ -9,7 +10,7 @@ print(a)
 
 b = 100
 # make it smaller by 7
-
+b -= 7
 
 print(b)
 
@@ -19,7 +20,7 @@ print(b)
 c = 44
 # please double c's value
 
-
+c *= 2
 print(c)
 
 
@@ -28,7 +29,7 @@ print(c)
 d = 125
 # please divide by 5 d's value 
 
-
+d /= 5
 print(d)
 
 
@@ -37,7 +38,7 @@ print(d)
 e = 8
 # please cube of e's value
 
-
+e **= 2 
 print(e)
 
 
@@ -46,40 +47,36 @@ print(e)
 f1 = 123
 f2 = 345
 # tell if f1 is bigger than f2 (pras a boolean)
-
-
-
+print(f1 > f2)
 
 
 g1 = 350
 g2 = 200
 # tell if the double of g2 is bigger than g1 (pras a boolean)
-
-
+print(2*g2 > g1)
 
 
 h = 1357988018575474
 # tell if it has 11 as a divisor (pras a boolean)
-
-
+print(h%11 == 0)
 
 
 
 i1 = 10
 i2 = 3
 # tell if i1 is higher than i2 squared and smaller than i2 cubed (pras a boolean)
-
+print(i2**3 > i1 > i2**2)
 
 
 
 j = 1521
 # tell if j is dividable by 3 or 5 (pras a boolean)
-
+print(j%3 == 0 or j%5 == 0)
 
 
 
 k = "Apple"
 #fill the k variable with its cotnent 4 times
-
+k = k * 4
 
 print(k)
