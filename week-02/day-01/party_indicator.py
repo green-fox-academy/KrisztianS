6cbdfa73a7525e7976429fb1feb29e8c5b5d6210
Krisztian Szucs <szucs.krisztian0818@gmail.com>
2017-09-11 17:34:35 +0200

    feat(party_indicator)exercise done

--- week-02/day-01/party_indicator.py
+++ week-02/day-01/party_indicator.py
@@ -11,4 +11,15 @@
 # If there are less people coming than 20
 #
 # It should print: Sausage party
-# If no girls are coming, regardless the count of the people
+# If no girls are coming, regardless the count of the people\
+
+girls = int(input("Girls: "))
+boys = int(input("Boys: "))
+if girls == boys and girls + boys >= 20:
+    print("The party is excellent!")
+if girls + boys > 20 and girls / boys != 1:
+    print("Quite cool party!")
+if girls + boys < 20:
+    print("Average pary...")
+if girls == 0:
+    print("Sausage party")
