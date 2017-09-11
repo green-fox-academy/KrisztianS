db328933edd2f7768887de6b62202f80bdf08286
Krisztian Szucs <szucs.krisztian0818@gmail.com>
2017-09-11 16:18:32 +0200

    feat(one_two_a_lot)exercise done

--- week-02/day-01/one_two_a_lot.py
+++ week-02/day-01/one_two_a_lot.py
@@ -3,3 +3,12 @@
 # If the number is one it should print: One
 # If the number is two it should print: Two
 # If the number is more than two it should print: A lot
+x = input("Give me a number! ")
+if int(x) <= 0:
+    print("Not enough")
+if int(x) == 1:
+    print("One")
+if int(x) == 2:
+    print("Two")
+if int(x) >= 2:
+    print("A lot")
