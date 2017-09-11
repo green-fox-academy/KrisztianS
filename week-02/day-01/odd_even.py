98cedf7e73bd4486282e079910505c38bd8bf0f6
Krisztian Szucs <szucs.krisztian0818@gmail.com>
2017-09-11 16:09:59 +0200

    feat(odd_even)exercise done

--- week-02/day-01/odd_even.py
+++ week-02/day-01/odd_even.py
@@ -1,2 +1,7 @@
 # Write a program that reads a number form the standard input,
 # Than prints "Odd" if the number is odd, or "Even" it it is even.
+x = input("Give me a number: ")
+if int(x) % 2 == 0:
+    print("Its even!")
+else:
+    print("Its odd!")
