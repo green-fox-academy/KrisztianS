17933478d83b54bdeb73a288c91bf115956a28af
Krisztian Szucs <szucs.krisztian0818@gmail.com>
2017-09-12 12:16:03 +0200

    feat(sum)exercise done

---
+++ week-02/day-02/sum.py
@@ -0,0 +1,10 @@
+# - Write a function called `sum` that sum all the numbers
+#   until the given parameter
+
+numbers = [20, 10, 100]
+
+def sums(numbers):
+    summary = sum(numbers)
+    return summary
+
+print(sums(numbers))
