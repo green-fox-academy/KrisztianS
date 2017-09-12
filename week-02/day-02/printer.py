f62cc01ff202b994399e8ca863c93f062bae1e01
Krisztian Szucs <szucs.krisztian0818@gmail.com>
2017-09-12 12:59:35 +0200

    feat(printer)exercise done

---
+++ week-02/day-02/printer.py
@@ -0,0 +1,8 @@
+# - Create a function called `printer`
+#   which prints the input parameters
+#   (can have multiple number of arguments)
+input = "hello"
+def printer(input):
+    layout = print(input)
+    return layout
+printer(input)
