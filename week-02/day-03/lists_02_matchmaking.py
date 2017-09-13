a3435dbbb3f4bd17d91dc9fbd5e2f9ffc31c9e8c
Krisztian Szucs <szucs.krisztian0818@gmail.com>
2017-09-13 15:33:21 +0200

    feat(lists_02_matchmaking): done

---
+++ week-02/day-03/lists_02_matchmaking.py
@@ -0,0 +1,13 @@
+# Join the two lists by matching one girl with one boy in the order list
+# Exepected output: ["Eve", "Joe", "Ashley", "Fred"...]
+
+girls = ["Eve", "Ashley", "Bözsi", "Kat", "Jane"]
+boys = ["Joe", "Fred", "Béla", "Todd", "Neef", "Jeff"]
+
+one_line = ""
+for i in range(len(girls)):
+    order = girls[i] + "," + boys[i] + ","
+    one_line += order
+
+
+print(one_line)
