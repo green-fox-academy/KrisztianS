97a0b81910098e4edf55e6e774a055ba14d6dc1c
Krisztian Szucs <krisztians0818@gmail.com>
2017-09-14 16:08:21 +0200

    feat(lists_08_calculator): exercise done!

---
+++ week-02/day-04/lists_08_calculator.py
@@ -0,0 +1,36 @@
+# Create a simple calculator application which does read the parameters from the prompt 
+# and prints the result to the prompt. 
+
+# It should support the following operations: 
+# +, -, *, /, % and it should support two operands. 
+
+# The format of the expressions must be: {operation} {operand} {operand}. 
+# Examples: "+ 3 3" (the result will be 6) or "* 4 4" (the result will be 16)
+
+# You should use the input() function to accept user input
+# It should work like this:
+
+# Start the program
+# It prints: "Please type in the expression:"
+# Waits for the user input
+# Print the result
+# Exit
+print("Please type in the expression: ")
+num_input = input()
+operation = num_input[0]
+operand1 = int(num_input[2])
+operand2 = int(num_input[4])
+
+def calculator(operation,operand1,operand2):
+    if operation == "+":
+        return operand1 + operand2
+    if operation == "-":
+        return operand1 - operand2
+    if operation == "*":
+        return operand1 * operand2
+    if operation == "/":
+        return operand1 / operand2
+    if operation == "%":
+        return operand1 % operand2
+    
+print(calculator(operation,operand1,operand2))
