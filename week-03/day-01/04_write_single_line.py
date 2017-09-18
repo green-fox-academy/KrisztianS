3c532a69a2be23755d7b134d0df4a2c1932fefee
Krisztian Szucs <krisztians0818@gmail.com>
2017-09-18 13:35:50 +0200

    feat(04_write_single_line): exercise done

---
+++ week-03/day-01/04_write_single_line.py
@@ -0,0 +1,11 @@
+# Open a file called "my-file.txt"
+# Write your name in it as a single line
+# If the program is unable to write the file,
+# then it should print an error message like: "Unable to write file: my-file.txt"
+try:
+    file = open("my-file.txt", "w")
+    file.write("Krisztian Szucs")
+    file.close()
+except IOError:
+    print("Unable to write file:" + file)
+    file.close()
