0a9fdb56e7f1c0b0f411cde02cb9f3ab8706621e
Krisztian Szucs <krisztians0818@gmail.com>
2017-09-18 17:36:42 +0200

    feat(07_decrypt): decrypting in progress...

---
+++ week-03/day-01/07_duplicated_chars.py
@@ -0,0 +1,10 @@
+# Create a method that decrypts the duplicated-chars.txt
+crypted = "week-03/day-01/duplicated-chars.txt"
+def decrypt(file_name):
+    decripted = ""
+    with open(file_name , "r") as f:
+        whole_file = f.read()
+        for i in range(len(whole_file)-1):
+            
+            decripted.append 
+decrypt()
