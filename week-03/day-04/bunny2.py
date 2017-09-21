430585d36a8083624df6fd14163402b10ac4a748
Krisztian Szucs <krisztians0818@gmail.com>
2017-09-21 12:11:30 +0200

    feat(06 bunny ears in Chernobil): exercise done

--- week-03/day-04/bunny2.py
+++ week-03/day-04/bunny2.py
@@ -2,3 +2,12 @@
 # (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
 # have 3 ears, because they each have a raised foot. Recursively return the
 # number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+def bunny_ears_in_Chernobil(bunnies):
+    if bunnies == 1:
+        return 2
+    elif bunnies % 2 == 0:
+        return 3 + bunny_ears_in_Chernobil(bunnies-1)
+    else:
+        return 2 + bunny_ears_in_Chernobil(bunnies-1)
+
+print(bunny_ears_in_Chernobil(2))
