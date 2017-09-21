7b7be629e9dd871bb8bf4ca6f3ef738fe1e84230
Krisztian Szucs <krisztians0818@gmail.com>
2017-09-21 14:59:56 +0200

    feat(07 string1 x to y): exercise done

--- week-03/day-04/string1.py
+++ week-03/day-04/string1.py
@@ -1,2 +1,15 @@
 # Given a string, compute recursively (no loops) a new string where all the
 # lowercase 'x' chars have been changed to 'y' chars.
+def x_to_y_changer(string):
+    new_string = ""
+    if len(string) == 0:
+        return new_string
+    else:
+        if string[0] == "x":
+            new_string = new_string + "y"
+            return new_string + x_to_y_changer(string[1:])
+        else:
+            new_string = new_string + string[0]
+            return new_string + x_to_y_changer(string[1:])
+
+print(x_to_y_changer("xxx"))
