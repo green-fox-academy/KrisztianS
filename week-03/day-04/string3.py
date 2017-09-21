4398e9d60a9a612039d78d5788a98f6942482d01
Krisztian Szucs <krisztians0818@gmail.com>
2017-09-21 15:08:04 +0200

    feat(09 string3 * between chars): exercise done

--- week-03/day-04/string3.py
+++ week-03/day-04/string3.py
@@ -1,2 +1,11 @@
 # Given a string, compute recursively a new string where all the
 # adjacent chars are now separated by a "*".
+def x_to_y_changer(string):
+    new_string = ""
+    if len(string) == 0:
+        return new_string
+    else:
+        new_string = new_string + string[0] + "*"
+        return new_string + x_to_y_changer(string[1:])
+
+print(x_to_y_changer("stars everywhere"))
