0b3ce44963b3ca33f80f43c5512d40638ebfbca3
Krisztian Szucs <krisztians0818@gmail.com>
2017-09-24 21:41:01 +0200

    feat(trees homework): done

---
+++ week-04/day-01/trees.py
@@ -0,0 +1,7 @@
+trees = [
+    {type : birch, leaf_color: brown, age : 28, sex: female}
+    {type: maple, leaf_color: gray, age: 25, sex: male}
+    {type : oak, leaf_color: brown, age : 85, sex: female}
+    {type: pine, leaf_color: gray, age: 15, sex: male}
+    {type : willow, leaf_color: brown, age : 5, sex: female}
+]
