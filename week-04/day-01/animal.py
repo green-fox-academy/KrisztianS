1a0b9353f1b2f37fad724dd0f7e0e2a8a53c89cb
Krisztian Szucs <krisztians0818@gmail.com>
2017-09-25 15:47:05 +0200

    feat(animals): exercise done

---
+++ week-04/day-01/animal.py
@@ -0,0 +1,29 @@
+# Create Animal class
+# Every animal has a hunger value, which is a whole number
+# Every animal has a thirst value, which is a whole number
+# when creating a new animal object these values are created with the default 50 value
+# Every animal can eat() which decreases their hunger by one
+# Every animal can drink() which decreases their thirst by one
+# Every animal can play() which increases both by one
+
+class Animal(object):
+    hunger = 50
+    thirst = 50
+
+    def eat(self):
+        self.hunger -= 1
+    def drink(self):
+        self.thirst -= 1
+    def play(self):
+        self.hunger += 1
+        self.thirst += 1
+
+dog = Animal()
+cat = Animal()
+sloth = Animal()
+
+sloth.play()
+sloth.eat()
+sloth.drink()
+
+print(sloth.hunger, sloth.thirst)
