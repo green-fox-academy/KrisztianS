b35c7c083eac0ef315376727574cdf7ace68e8aa
Krisztian Szucs <krisztians0818@gmail.com>
2017-09-25 14:42:32 +0200

    feat(dices): exercise done

---
+++ week-04/day-01/dice.py
@@ -0,0 +1,36 @@
+import random
+
+class Dice(object):
+
+    def __init__(self):
+        self.dice = [0, 0, 0, 0, 0, 0]
+
+    def roll(self):
+        for i in range(len(self.dice)):
+            self.dice[i] = random.randint(1,6)
+        return self.dice
+
+    def get_current(self, index=None):
+        if index != None:
+            return self.dice[index]
+        else:
+            return self.dice
+
+    def reroll(self, index=None):
+        if index != None:
+            self.dice[index] = random.randint(1,6)
+        else:
+            self.roll()
+
+
+my_dices = Dice()
+# print(dice.get_current())
+# dice.roll()
+# print(dice.get_current())
+# dice.reroll()
+# print(dice.get_current())
+print(my_dices.dice)
+for i in range(len(my_dices.dice)):
+    while (my_dices.get_current(i)) != 6:
+        my_dices.reroll(i)
+print(my_dices.dice)
