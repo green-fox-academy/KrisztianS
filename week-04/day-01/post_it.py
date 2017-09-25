b432067b7f1a0d63e28d71a72380c8ea7632e89e
Krisztian Szucs <krisztians0818@gmail.com>
2017-09-25 11:59:01 +0200

    feat(post it): exercise done

---
+++ week-04/day-01/post_it.py
@@ -0,0 +1,21 @@
+# Create a PostIt class that has
+# a background_color
+# a text on it
+# a text_color
+# Create a few example post-it objects:
+# an orange with blue text: "Idea 1"
+# a pink with black text: "Awesome"
+# a yellow with green text: "Superb!"
+
+class PostIt(object):
+
+    def __init__(self, background_color, text, text_color):
+        self.background_color = background_color
+        self.text = text
+        self.text_color = text_color
+
+PostIt_orange = PostIt("orange", "Idea 1", "blue")
+PostIt_pink = PostIt("pink", "Awesome", "black")
+PostIt_yellow = PostIt("pink", "Awesome", "black")
+
+print(PostIt_orange.text)
