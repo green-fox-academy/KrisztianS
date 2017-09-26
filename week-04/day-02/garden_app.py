bb4536bf13325e3ea8f57f5da4d17e4fd66edf28
Krisztian Szucs <krisztians0818@gmail.com>
2017-09-26 17:36:04 +0200

    feat(garden): started watering function

--- week-04/day-02/garden_app.py
+++ week-04/day-02/garden_app.py
@@ -7,8 +7,19 @@ class Garden(object):
         self.plants.append(plant)
 
 class Tree(object):
+
     def __init__(self, name):
         self.name = name
 
-class Flower(object):
-    pass
+    if water_amount < 10:
+        print("The {} needs water".format(self.name))
+    else:
+        print("The {} doesnt nedd water".format(self.name))
+
+class Flower(Tree):
+    
+    if water_amount < 5:
+        print("The {} nedds water". format(self.name))
+    else:
+        print("The {} doesnt nedd water".format(self.name))
+    
