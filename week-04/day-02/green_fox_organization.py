8884bbad5e98a82680f11f843cbfec31f7de638a
Krisztian Szucs <krisztians0818@gmail.com>
2017-09-26 15:21:27 +0200

    fix(green fox organization): sponsor class parameters fixed

--- week-04/day-02/green_fox_organization.py
+++ week-04/day-02/green_fox_organization.py
@@ -37,10 +37,10 @@ class Mentor(Person):
 
 class Sponsor(Person):
     
-    def __init__(self, name="Jane Doe", age=30, gender="female", company="Google" , hired_students=0):
+    def __init__(self, name="Jane Doe", age=30, gender="female", company="Google"):
         super().__init__(name, age, gender)
         self.company = company
-        self.hired_students = hired_students
+        self.hired_students=0
 
     def introduce(self):
          print("Hi, I'm {}, a {} year old {} who represents {} and hired {} students so far.".format(self.name, self.age, self.gender, self.company, self.hired_students))
