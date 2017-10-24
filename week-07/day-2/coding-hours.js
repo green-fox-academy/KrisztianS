653d494da39b9c3dcdf41a1654415d2194abe3ef
Krisztian Szucs <krisztians0818@gmail.com>
2017-10-24 11:57:59 +0200

    feat(6. exercise) exercise done

---
+++ week-07/day-2/coding-hours.js
@@ -0,0 +1,5 @@
+'use strict';
+
+let coding_hours = 6 * 5 * 17;
+console.log('An average attendee codes:' , coding_hours, 'hours.');
+console.log('The average percentage of coding hours in a semester is' , Math.round(coding_hours / (52 * 17) *100), '%.');
