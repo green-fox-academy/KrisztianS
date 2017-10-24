0ea93893dafe2f4815c6a1cb9d0aed177895d3ce
Krisztian Szucs <krisztians0818@gmail.com>
2017-10-24 15:26:44 +0200

    feat(exercise 24) done

---
+++ week-07/day-2/print-even.js
@@ -0,0 +1,8 @@
+'use strict';
+
+for (let i = 0; i <= 500; i += 1) {
+    if (i % 2 == 0 && i !== 0) {
+        console.log(i)
+    }
+}
+// Create a program that prints all the even numbers between 0 and 500
