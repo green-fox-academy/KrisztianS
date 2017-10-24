b68a0105944bb026f717f8475b930b1332439c0f
Krisztian Szucs <krisztians0818@gmail.com>
2017-10-24 15:19:05 +0200

    feat(exercise 23) done

---
+++ week-07/day-2/wont-cheat.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let oath = 'I won\'t cheat on the exam!'
+
+for (let i=0; i < 100; i +=1) {
+    console.log(oath)
+}
+// Create a program that writ'es this line 100 times:
+//"I won't cheat on the exam!"
