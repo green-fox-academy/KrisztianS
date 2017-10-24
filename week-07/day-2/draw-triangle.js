3bad913cea465bf04bf39571f34110f85db753f6
Krisztian Szucs <krisztians0818@gmail.com>
2017-10-24 15:51:58 +0200

    feat(exercise 28) done

---
+++ week-07/day-2/draw-triangle.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var lineCount = 104;
+var star = '*'
+
+for (let i = 1; i <= lineCount; i++) {
+    console.log(star.repeat(i))
+}
+// Write a program that draws a
+// triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as lineCount is
