e2fc86fa09e5d70c7d30bb803e911c5d7bbbd14d
Krisztian Szucs <krisztians0818@gmail.com>
2017-10-25 17:32:43 +0200

    feat(01/obj student counter) started to create a function

---
+++ week-07/day-3/01_obj_student-counter.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var students = [
+        {'name': 'Teodor', 'age': 3, 'candies': 2},
+        {'name': 'Rezso', 'age': 9.5, 'candies': 2},
+        {'name': 'Zsombor', 'age': 12, 'candies': 5},
+        {'name': 'Aurel', 'age': 7, 'candies': 3},
+        {'name': 'Olaf', 'age': 12, 'candies': 7},
+        {'name': 'Gerzson', 'age': 10, 'candies': 1},
+]
+console.log(students[0].name)
+
+function sum_candies(student_list) {
+    var candies = 0
+    for (var i = 0; i < student_list.length; i++) {
+        
+    }
+    return candies
+}
+
+console.log(sum_candies('Teodor', 'Rezso'))
+
+// create a function that takes a list of students and logs: 
+// - how many candies are owned by students
+
+// create a function that takes a list of students and logs:
+// - Sum of the age of people who have lass than 5 candies
