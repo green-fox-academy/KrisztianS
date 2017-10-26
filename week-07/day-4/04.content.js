6fa3189d59f6cc449c98a48a649456d56961274b
Krisztian Szucs <krisztians0818@gmail.com>
2017-10-26 14:51:10 +0200

    feat(04.content exercise) done

---
+++ week-07/day-4/04.content.js
@@ -0,0 +1,7 @@
+let output1 = document.querySelector('.output1')
+
+output1.innerHTML = document.getElementsByTagName('p')[0].textContent
+
+let output2 = document.querySelector('.output2')
+
+output2.innerHTML = document.getElementsByTagName('p')[0].innerHTML
