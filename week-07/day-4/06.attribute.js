dd924bc4c1f9f563fc705e86d0ad8d7c9336e912
Krisztian Szucs <krisztians0818@gmail.com>
2017-10-26 15:48:08 +0200

    feat(06.attribute exercise) done

---
+++ week-07/day-4/06.attribute.js
@@ -0,0 +1,7 @@
+console.log(document.getElementsByTagName('img')[0].src)
+
+document.getElementsByTagName('img')[0].src = 'https://images.jg-cdn.com/image/b04c0749-7d1b-426d-adcd-70a4d0bec075.jpg?autorotate=true'
+
+document.getElementsByTagName('a')[0].href = 'https://www.greenfoxacademy.com/'
+
+document.querySelector('.this-one').textContent = 'Don\'t click me!' 
