e0adc8286737b15988fa7d388737e1df006632d5
Krisztian Szucs <krisztians0818@gmail.com>
2017-11-03 14:44:45 +0100

    feat(reddit frontend) articles loaded and created as anchors temporarily

--- week-08/day-5/reddit.js
+++ week-08/day-5/reddit.js
@@ -1,18 +1,26 @@
 'use strict'
 
 var config = 'https://cors-anywhere.herokuapp.com/http://secure-reddit.herokuapp.com/posts'
+var articles_div = document.querySelector('.articles')
 
 function doRequest(callback) {
     var x = new XMLHttpRequest();
     x.open('GET', config);
     x.onload = function() {
-        callback(x.responseText)
+        callback(JSON.parse(x.responseText))
     };
     x.send();
   }
 
   function handleData(data){
     console.log(data);
+    data.posts.forEach(function(article) {
+        let single_article = document.createElement('a')
+        let url = article.url
+        single_article.href = url
+        single_article.innerHTML = "dads"
+        articles_div.appendChild(single_article)
+    })
   }
 
   doRequest(handleData)
