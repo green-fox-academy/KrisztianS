940d6b71b434059eaad58b789dbd0942ddf97a82
Krisztian Szucs <krisztians0818@gmail.com>
2017-11-08 15:37:33 +0100

    feat(rest, backend project) doubling function works

--- week-09/day-3/index.js
+++ week-09/day-3/index.js
@@ -2,9 +2,26 @@ var express = require('express');
 var app = express();
 
 app.use('/assets', express.static('./assets'))
+express.json.type = "application/json"
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 })
 
-app.listen(3000);
+app.get('/doubling', function(req, res){
+    if (req.query.input == null){
+        res.send({
+            "error": "Please provide an input!"
+          })
+    } else {
+        res.send(
+            {
+                "received": req.query.input,
+                "result": req.query.input * 2
+              }
+        )
+    }
+    
+})
+
+app.listen(8080);
